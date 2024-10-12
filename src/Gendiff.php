c0c8b65d817989a26b89f8705c236aae3948541b
Kreoin <steamuser85@yandex.ru>
2024-10-12 20:03:31 +0300

    modify genDiff

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -1,14 +1,47 @@
 <?php
 
-namespace App\Gendiff;
+namespace Differ\Differ;
 
-function generate($format, $filepath1 , $filepath2): void {
-    echo $format . ' ' . $filepath1 . ' ' . $filepath2 . "\n";
+function genDiff($filepath1 , $filepath2, $format = 'stylish'): void 
+{
+    //echo $format . ' ' . $filepath1 . ' ' . $filepath2 . "\n";
     $filecontent1 = file_get_contents(realpath($filepath1));
     $filecontent2 = file_get_contents(realpath($filepath2));
-    echo $filecontent1 . "\n" . $filecontent2;
-    $json1 = json_decode($filecontent1);
-    $json2 = json_decode($filecontent2);
-    print_r($json1);
-    print_r($json2);
+    //echo "{$filecontent1}\n\n{$filecontent2}\n\n";
+    $json1 = json_decode($filecontent1, true, 2);
+    $json2 = json_decode($filecontent2, true, 2);
+    $keys = array_keys([...$json1, ...$json2]);
+    asort($keys);
+    $keys = array_values($keys);
+    $resultDiff = [];
+    foreach ($keys as $key) {
+        $keyExist1 = array_key_exists($key, $json1);
+        $keyExist2 = array_key_exists($key, $json2);
+        if ($keyExist1 && $keyExist2) {
+            if($json1[$key] === $json2[$key]) {
+                $resultDiff[] = "  {$key}: " . getValue($json1, $key);
+            } else {
+                $resultDiff[] = "- {$key}: " . getValue($json1, $key);
+                $resultDiff[] = "+ {$key}: " . getValue($json2, $key);
+            }
+        } elseif ($keyExist1) {
+            $resultDiff[] = "- {$key}: " . getValue($json1, $key);
+        } else {
+            $resultDiff[] = "+ {$key}: " . getValue($json2, $key);
+        }
+    }
+    print_r(implode("\n",$resultDiff));
+    print_r("\n");
 }
+
+function getValue($assoc, $key) 
+{
+    $value = $assoc[$key];
+    if ($value == 1) {
+        return 'true';
+    } 
+    if ($value == '') {
+        return 'false';
+    }
+    return $value;
+};
