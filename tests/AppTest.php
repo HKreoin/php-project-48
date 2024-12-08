3f7c0b8fd45fbfadb39ee4ec5446d1e65b51d512
Kreoin <steamuser85@yandex.ru>
2024-12-08 23:01:13 +0300

    fix linter

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -12,9 +12,9 @@ class AppTest extends TestCase
     public function testGendiffJson(): void
     {
         $filePath1 = __DIR__ . "/fixtures/file1.json";
-        $filePath2= __DIR__ . "/fixtures/file2.json";
+        $filePath2 = __DIR__ . "/fixtures/file2.json";
         $expectedPath = __DIR__ . "/fixtures/result.txt";
-        
+
         $expected = parse($expectedPath);
         $actual = genDiff($filePath1, $filePath2);
         $this->assertEquals($expected, $actual);
@@ -23,11 +23,22 @@ class AppTest extends TestCase
     public function testGendiffYaml(): void
     {
         $filePath1 = __DIR__ . "/fixtures/file1.yaml";
-        $filePath2= __DIR__ . "/fixtures/file2.yml";
+        $filePath2 = __DIR__ . "/fixtures/file2.yml";
         $expectedPath = __DIR__ . "/fixtures/result.txt";
-        
+
         $expected = parse($expectedPath);
         $actual = genDiff($filePath1, $filePath2);
         $this->assertEquals($expected, $actual);
     }
+
+    /* public function testGendiffJson2(): void
+    {
+        $filePath1 = __DIR__ . "/fixtures/file3.json";
+        $filePath2= __DIR__ . "/fixtures/file3.json";
+        $expectedPath = __DIR__ . "/fixtures/result2.txt";
+        
+        $expected = parse($expectedPath);
+        $actual = genDiff($filePath1, $filePath2);
+        $this->assertEquals($expected, $actual);
+    } */
 }
