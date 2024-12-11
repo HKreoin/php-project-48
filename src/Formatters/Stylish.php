edfd3b9c9fa0f2d34ef7e3a5686aa62fdd7c5489
Kreoin <steamuser85@yandex.ru>
2024-12-11 19:16:48 +0300

    refactor Stylish.php

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -2,25 +2,34 @@
 
 namespace Differ\Formatters\Stylish;
 
-function format($content)
+function format(array $content): string
 {
-    $decore = [
-        'unchanged' => function ($item) {
-            return "    {$item['key']}: {$item['value']}";
-        },
-        'changed' => function ($item) {
-            return "  - {$item['key']}: {$item['value1']}\n  + {$item['key']}: {$item['value2']}";
-        },
-        'added' => function ($item) {
-            return "  + {$item['key']}: {$item['value']}";
-        },
-        'removed' => function ($item) {
-            return "  - {$item['key']}: {$item['value']}";
-        },
-    ];
+    return "{\n" . formatData($content) . "\n}";
+}
 
-    $diff = array_map(fn($item) => $decore[$item['type']]($item), $content);
-    $result = "{\n" . implode("\n", $diff) . "\n}";
+function formatData(array $content, int $level = 0): string
+{
+    $spaces = str_repeat('    ', $level);
+    return implode("\n", array_map(function ($item) use ($spaces, $level) {
+        $key = $item['key'];
+        $type = $item['type'];
+        $value = formatValue($item['value'], $level + 1);
+        $oldValue = $type === 'changed' ? formatValue($item['oldValue'], $level + 1) : null;
+        return match ($type) {
+            'added' => "{$spaces}  + {$key}: {$value}",
+            'removed' => "{$spaces}  - {$key}: {$value}",
+            'unchanged' => "{$spaces}    {$key}: {$value}",
+            'changed' => "{$spaces}  - {$key}: {$oldValue}\n{$spaces}  + {$key}: {$value}",
+        };
+    }, $content));
+}
 
-    return $result;
+function formatValue($value, int $level): string
+{
+    if (is_array($value)) {
+        $data = formatData($value, $level);
+        $spaces = str_repeat('    ', $level);
+        return "{\n{$data}\n{$spaces}}";
+    }
+    return is_string($value) ? $value : json_encode($value);
 }
