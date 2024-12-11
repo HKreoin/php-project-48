7c0457057dbb8b105fc433850e9bdd0182016847
Kreoin <steamuser85@yandex.ru>
2024-12-11 20:47:22 +0300

    plain format test

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -41,4 +41,15 @@ class AppTest extends TestCase
         $actual = genDiff($filePath1, $filePath2);
         $this->assertEquals($expected, $actual);
     }
+
+    public function testGendiffJsonPlain(): void
+    {
+        $filePath1 = __DIR__ . "/fixtures/file3.json";
+        $filePath2 = __DIR__ . "/fixtures/file4.json";
+        $expectedPath = __DIR__ . "/fixtures/resultPlain.txt";
+
+        $expected = parse($expectedPath);
+        $actual = genDiff($filePath1, $filePath2, 'plain');
+        $this->assertEquals($expected, $actual);
+    }
 }
