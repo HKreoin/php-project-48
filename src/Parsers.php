f0a69b166da7cbfa7a09d1c0540d0b17871dc037
Kreoin <steamuser85@yandex.ru>
2024-12-17 20:28:53 +0300

    fix Parsers.php

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,8 +6,11 @@ use Symfony\Component\Yaml\Yaml;
 
 function parse(string $filepath): mixed
 {
-    $filecontent = (string) file_get_contents($filepath) ??
+    try {
+        $filecontent = (string) file_get_contents($filepath);
+    } catch (\Exception $e) {
         throw new \Exception("File not found: $filepath");
+    }
     $extension = explode('.', $filepath)[1];
     return match ($extension) {
         'json' => json_decode($filecontent, true),
